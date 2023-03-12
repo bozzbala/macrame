2deedd05bb595d5101a7b69a33cbb38ddc34d0be
Temirlan <flashtima@gmail.com>
2023-03-12 15:05:52 +0600

    products

--- product.php
+++ product.php
@@ -1,13 +1,56 @@
-<main>
-    <section class="intro">
-        <div class="intro-header">БОЛЬШОЙ АССОРТИМЕНТ ТОВАРОВ</div>
-        <p>Всё что нужно для создания макраме</p>
-        <a href="#">
-            <div class="button-main">К ТОВАРАМ</div>
-        </a>
-    </section>
+<!DOCTYPE html>
+<html lang="en">
 
-    <section class="products-wrapper">
-        <div class=""></div>
-    </section>
-</main>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="product.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+    <?php include 'header.php' ?>
+    <main>
+        <section class="intro">
+            <div class="intro-header">БОЛЬШОЙ АССОРТИМЕНТ ТОВАРОВ</div>
+            <p>Всё что нужно для создания макраме</p>
+            <a href="#">
+                <div class="button-main">К ТОВАРАМ</div>
+            </a>
+        </section>
+
+        <section class="products-wrapper">
+            <div class="product-category">ШНУРЫ</div>
+            <div class="products">
+                <div class="product">
+                    <div class="product-img"><img src="product.png"></div>
+                    <div class="product-title">Название</div>
+                    <div class="product-price">19.99$</div>
+                    <div class="product-button">В КОРЗИНУ</div>
+                </div>
+                <div class="product">
+                    <div class="product-img"><img src="product.png"></div>
+                    <div class="product-title">Название</div>
+                    <div class="product-price">19.99$</div>
+                    <div class="product-button">В КОРЗИНУ</div>
+                </div>
+                <div class="product">
+                    <div class="product-img"><img src="product.png"></div>
+                    <div class="product-title">Название</div>
+                    <div class="product-price">19.99$</div>
+                    <div class="product-button">В КОРЗИНУ</div>
+                </div>
+                <div class="product">
+                    <div class="product-img"><img src="product.png"></div>
+                    <div class="product-title">Название</div>
+                    <div class="product-price">19.99$</div>
+                    <div class="product-button">В КОРЗИНУ</div>
+                </div>
+            </div>
+        </section>
+    </main>
+    <?php include 'footer.php' ?>
+</body>
+
+</html>
