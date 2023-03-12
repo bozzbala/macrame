df388c9531ebac5cfe7543569b97cf4e6877a1e4
NiyuShion <126596226+NiyuShion@users.noreply.github.com>
2023-03-12 14:34:27 +0600

    Add files via upload

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Document</title>
 	<link href="style.css" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
 </head>
 
 <body>
@@ -88,6 +89,45 @@
 			</div>
 		</section>
 	</main>
+
+	<footer class="footer">
+  	 <div class="container">
+  	 	<div class="row">
+  	 	<div class="footer-col">
+  	 			<h4>Социальные Сети</h4>
+  	 			<div class="social-links">
+  	 				<a href="#"><i class="fab fa-facebook-f"></i></a>
+  	 				<a href="#"><i class="fab fa-twitter"></i></a>
+  	 				<a href="#"><i class="fab fa-instagram"></i></a>
+  	 			</div>
+  	 		</div>
+  	 		<div class="footer-col">
+  	 			<h4>Контакты</h4>
+				<div class="footer-tex">
+  	 			<ul>
+  	 				<li>Алматы, ул. Желтоксан 115,БЦ Кайсар Плаза, 2 этаж</a></li>
+  	 				<li>Время работы: 10:00 - 19:00</a></li>
+  	 				<li>+7 778 746 38 01</a></li>
+  	 			</ul>
+				</div>	
+  	 		</div>
+  	 		<div class="footer-col">
+  	 			<h4>Ссылки</h4>
+  	 			<ul>
+  	 				<li><a href="#">Видеоуроки</a></li>
+  	 				<li><a href="#">Магазин</a></li>
+  	 				<li><a href="#">Материалы</a></li>
+  	 				<li><a href="#">Обо мне</a></li>
+					<li><a href="#">Контакты</a></li>
+  	 			</ul>
+  	 		</div>
+  	 		
+  	 	</div>
+  	 </div>
+  </footer>
+
+
 </body>
+
 
 </html>
