75ae55c6524555ea09d5c29c83ab7b9798b9ff27
Temirlan <flashtima@gmail.com>
2023-03-12 14:35:03 +0600

    Product Created

---
+++ product.php
@@ -0,0 +1,13 @@
+<main>
+    <section class="intro">
+        <div class="intro-header">БОЛЬШОЙ АССОРТИМЕНТ ТОВАРОВ</div>
+        <p>Всё что нужно для создания макраме</p>
+        <a href="#">
+            <div class="button-main">К ТОВАРАМ</div>
+        </a>
+    </section>
+
+    <section class="products-wrapper">
+        <div class=""></div>
+    </section>
+</main>
