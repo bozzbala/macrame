02c6e307f8a82e550022d70d3d506b024f2b86e8
NiyuShion <126596226+NiyuShion@users.noreply.github.com>
2023-03-26 17:18:37 +0600

    Add files via upload

---
+++ BLL/newProduct.php
@@ -0,0 +1,26 @@
+<?php
+include 'BLL/db.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Получаем данные из формы
+    $name = mysqli_real_escape_string($mysqli, $_POST['name']);
+    $description = mysqli_real_escape_string($mysqli, $_POST['description']);
+    $price = mysqli_real_escape_string($mysqli, $_POST['price']);
+    $image_url = mysqli_real_escape_string($mysqli, $_POST['image_url']);
+    $category = mysqli_real_escape_string($mysqli, $_POST['category']);
+
+    // Создаем SQL-запрос для добавления товара в базу данных
+    $sql = "INSERT INTO `products` (`name`, `description`, `price`, `image_url`, `category`) 
+            VALUES ('$name', '$description', '$price', '$image_url', '$category')";
+
+    // Выполняем запрос
+    if (mysqli_query($mysqli, $sql)) {
+        echo "Товар успешно добавлен в базу данных.";
+    } else {
+        echo "Ошибка: " . mysqli_error($mysqli);
+    }
+
+    // Закрываем соединение с базой данных
+    mysqli_close($mysqli);
+}
+?>
