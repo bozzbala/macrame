6197257fa873f437c775f11b4799322834c7b124
NiyuShion <126596226+NiyuShion@users.noreply.github.com>
2023-03-29 18:00:16 +0600

    Add files via upload

--- BLL/db.php
+++ BLL/db.php
@@ -1,9 +1,10 @@
 <?php
 $mysqli = mysqli_connect("localhost", "root", "", "macrame");
-
+$conn = $mysqli;
 // Проверка соединения
 if (mysqli_connect_errno()) {
     printf("Ошибка соединения: %s\n", mysqli_connect_error());
     exit();
 }
+echo "connected";
 ?>
