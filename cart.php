8de1a1d279a15027f1ff0ba71f4b8a270ddad6a3
NiyuShion <126596226+NiyuShion@users.noreply.github.com>
2023-04-09 15:50:13 +0600

    Add files via upload

--- cart.php
+++ cart.php
@@ -44,15 +44,23 @@ if (!empty($cart_items)) {
                 </tr>
             </thead>
             <tbody>
-                <?php foreach ($products as $product) { ?>
-                    <tr>
-                        <td><img src="<?php echo "./db/" . strtok($product['image_url'], " "); ?>" width="50"></td>
-                        <td><?php echo $product['name']; ?></td>
-                        <td><?php echo $product['price']; ?></td>
-                        <td><?php echo $cart_items[$product['id']]['quantity']; ?></td>
-                        <td><?php echo $cart_items[$product['id']]['quantity'] * $product['price']; ?></td>
-                    </tr>
-                <?php } ?>
+            <?php foreach ($products as $product) { ?>
+    <tr>
+        <td><img src="<?php echo "./db/" . strtok($product['image_url'], " "); ?>" width="50"></td>
+        <td><?php echo $product['name']; ?></td>
+        <td><?php echo $product['price']; ?></td>
+        <td>
+            <form method="post" action="BLL/update_cart.php">
+                <input type="hidden" name="product_id" value="<?php echo $product['id']; ?>">
+                <input type="number" name="quantity" value="<?php echo $cart_items[$product['id']]['quantity']; ?>">
+                <button type="submit" name="action" value="increment">+</button>
+                <button type="submit" name="action" value="decrement">-</button>
+            </form>
+        </td>
+        <td><?php echo $cart_items[$product['id']]['quantity'] * $product['price']; ?></td>
+    </tr>
+<?php } ?>
+
             </tbody>
         </table>
         <p>Общая стоимость: <?php echo $total_price; ?></p>
